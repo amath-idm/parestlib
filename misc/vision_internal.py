e1bbdf07d173f80310d7e3340a2659603a930beb
cliffckerr <questionably@gmail.com>
2020-02-21 02:25:19 -0800

    modifying examples

--- misc/vision_internal.py
+++ misc/vision_internal.py
@@ -1,3 +1,40 @@
+#%% User interface
+
+import pylab as pl
+import emodpy as ep
+
+# Set the algorithm metaparameters
+algorithm = 'shellstep' # Which optimization algorithm to use
+how = 'parallel' # How to run (local, parallel, or COMPS)
+n_iters = 10 # Maximum iterations of calibration algorithm
+n_samples = 20 # Number of samples per iteration
+
+# Define the objective of the calibration
+def objective_fn(sim):
+    orig_infected = pl.array(orig_results.data['Channels']['Infected']['Data'])
+    this_infected = pl.array(sim.results.data['Channels']['Infected']['Data'])
+    mismatch = ((orig_infected - this_infected)**2).sum()
+    return mismatch
+
+# Choose which parameters to calibrate
+parameters = [{'name': 'Base_Infectivity',
+              'best': 0.0001,
+              'min': 0.0,
+              'max': 0.1}]
+
+# Run calibration
+sim = ep.Simulation()
+calib = ep.Calibration(sim=sim, parameters=parameters, objective_fn=objective_fn, algorithm=algorithm)
+results = calib.run(how=how, n_iters=n_iters)
+
+
+
+#%% Internal implementation
+
+import optim_methods as om
+from . import em_simulations as ems
+from . import em_experiments as ees
+
 class Calibration:
     
     def run(self, objective_fn=None, algorithm=None, ...):
