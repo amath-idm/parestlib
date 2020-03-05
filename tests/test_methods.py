0aaebdb065cf5dacb66728b40d9b916a6f7e0114
cliffckerr <questionably@gmail.com>
2020-03-04 16:30:07 -0800

    refactored test

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -4,12 +4,11 @@ Run tests on each of the methods.
 Optimizations run are problems * methods * repeats; comment out lines that aren't
 required.
 
-Version: 2019aug13
+Version: 2020mar04
 '''
 
 import sciris as sc
 import parestlib as om
-import numpy as np
 
 problems = [
     'norm',
@@ -25,6 +24,29 @@ methods = {
     'ASD':       om.asd,
     #'BSD':       om.bsd,
     'ShellStep': om.shellstep,
+}
+
+
+kwargs = {
+    'ASD': {},
+    'ShellStep': {
+        'xmin' : [-5, -5, -5],
+        'xmax' : [5, 5, 5], # TODO: Error if guess is outside of xmin / xmax
+        'maxiters': 20,
+        'mp' : {
+            'mu_r': 0.05,
+            'sigma_r': 0.01,
+            'N': 20,
+            'center_repeats': 1,
+            'rsquared_thresh': 0.5,
+            'useadaptation': True,
+            'adaptation': {
+                    'step': 0.9, # Should be less than 1
+                    'min': 0.05, # More than 0
+                    'max': 1.4
+            }
+        },
+    }
 }
 
 repeats = 3
@@ -49,32 +71,13 @@ for method,optim_func in methods.items():
 
             for r in range(repeats):
                 print('\nRun %s of %s with noise=%s:' % (n*repeats+r+1, repeats*len(noisevals), noise))
-                kwargs = {}
-                if method == 'ShellStep':
-                    kwargs = {
-                        'xmin' : [-5, -5, -5],
-                        'xmax' : [5, 5, 5], # TODO: Error if guess is outside of xmin / xmax
-                        'maxiters': 20,
-                        'mp' : {
-                            'mu_r': 0.05,
-                            'sigma_r': 0.01,
-                            'N': 20,
-                            'center_repeats': 1,
-                            'rsquared_thresh': 0.5,
-                            'useadaptation': True,
-                            'adaptation': {
-                                    'step': 0.9, # Should be less than 1
-                                    'min': 0.05, # More than 0
-                                    'max': 1.4
-                            }
-                        },
-                    }
-                result = optim_func(objective_func, startvals[problem], optimum='min', verbose=0, **kwargs)
+                
+                result = optim_func(objective_func, startvals[problem], optimum='min', verbose=0, **kwargs[method])
                 results.append(result)
                 # DJK N.B. Iterations is different from the number of funcation evaluations!
                 # In supercomputing, we also aim to reduce iterations
-                iters = np.shape(np.array(result.details.fvals))[0]
-                fevals = np.size(np.array(result.details.fvals))
+                iters = result.details.fvals.shape[0]
+                fevals = len(result.details.fvals)
                 print(  f'\tIterations: {iters}\n',
                         f'\tFunction Evaluations: {fevals}',
                         f'\tValue: {result.fval}',
