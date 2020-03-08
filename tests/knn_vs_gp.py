dc55a257188608e5b0e7f853de7bbaf0a8db609f
Daniel Klein <dklein@idmod.org>
2020-03-08 11:00:57 -0700

    Updating with my version of the script.  Noise is centered, the function is a ... function, and a few cleanups

--- tests/knn_vs_gp.py
+++ tests/knn_vs_gp.py
@@ -1,4 +1,3 @@
-import sciris as sc
 import pandas as pd
 import pylab as pl
 import parestlib as pe
@@ -6,32 +5,20 @@ from history_matching.gpr import GPR
 from history_matching.basis import Basis
 
 
-#%% Set up the problem
-sc.tic()
-
 # Set input data parameters
 ntrain = 200
 ntest  = 50
 npars  = 2
-noise  = 1.0
-seed   = 1
+noise  = 1
+
+def f(x):
+    return pl.sqrt(((x-0.5)**2).sum(axis=1))
 
 # Set up training and test arrays
-pl.seed(seed)
 train_arr = pl.rand(ntrain, npars)
+train_vals = f(train_arr) + noise*(pl.rand(ntrain)-0.5) # Distance from center
 test_arr = pl.rand(ntest, npars)
-train_vals = pl.sqrt(((train_arr-0.5)**2).sum(axis=1)) + noise*(pl.rand(ntrain)) # Distance from center
-test_vals = pl.sqrt(((test_arr-0.5)**2).sum(axis=1)) + noise*(pl.rand(ntest)) # Distance from center
-
-#%% KNN
-t1_knn = sc.tic()
-test_vals_knn = pe.bootknn(test=test_arr, train=train_arr, values=train_vals) 
-t2_knn = sc.toc(t1_knn, output=True)
-timestr_knn = f'KNN time = {t2_knn:0.5f} s'
-
-#%% GP
-
-t1_gp = sc.tic()
+test_vals = f(test_arr)
 
 par_names = [f'x{i}' for i in range(npars)]
 param_info = pd.DataFrame( {'Min':[0,0], 'Max':[1,1]}, index=par_names)
@@ -51,41 +38,34 @@ test_data.index.name = 'Sample_Id'
 basis = Basis.identity_basis(param_info.index, param_info=param_info)
 
 # Create an instance of the GPR
-gpr = GPR(basis, Ycol, train_data, param_info)
+gpr = GPR(basis, Ycol, train_data, param_info, log_transform=False)
 
 # Fit the GPR
 gpr.optimize_hyperparameters(
-    x0 = [1, 0.5] + [0.05]*npars,
-    bounds = ((0.1,100), (0.1,10)) + ((0.01,0.75),)*npars
+    x0 = [0.5, 0.5] + [0.1]*npars,
+    bounds = ((0.05,100), (0.1,10)) + ((0.01,1),)*npars
 )
 
 # Calculate the estimates
 ret = gpr.evaluate(test_data)
-print(ret)
 
-t2_gp = sc.toc(t1_gp, output=True)
-timestr_gp = f'GP time = {t2_gp:0.5f} s'
+test_vals_knn = pe.bootknn(test=test_arr, train=train_arr, values=train_vals) 
 
-
-
-#%% Plot results
+# Plot results
 fig, ax = pl.subplots(1,2,figsize=(16,10))
 
 if npars >= 2:
     ax[0].scatter(train_arr[:,0], train_arr[:,1], c=train_vals, marker='o')
-    ax[0].scatter(test_arr[:,0], test_arr[:,1], c=test_vals_knn.best, marker='s', s=35)
+    ax[0].scatter(test_arr[:,0], test_arr[:,1], c=test_vals_knn.best, marker='s', s=50)
     ax[0].scatter(test_arr[:,0], test_arr[:,1], c=ret['Mean'], marker='o')
     ax[0].set_xlabel(par_names[0])
     ax[0].set_ylabel(par_names[1])
 
 ax[1].plot([min(test_vals),max(test_vals)], [min(test_vals),max(test_vals)], 'r-')
-ax[1].scatter(test_vals, test_vals_knn.best, marker='s', label='KNN')
-ax[1].errorbar(x=test_vals, y=ret['Mean'], yerr=2*pl.sqrt(ret['Var_Latent']), fmt='o', c='g', lw=0.5, label='GP')
+ax[1].errorbar(x=test_vals, y=test_vals_knn.best, yerr=[test_vals_knn.low, test_vals_knn.high], fmt='o', c='b', lw=0.5, label='KNN')
+ax[1].errorbar(x=test_vals, y=ret['Mean'], yerr=2*pl.sqrt(ret['Var_Latent']), fmt='o', c='g', lw=1, label='GP')
 ax[1].legend()
 ax[1].set_xlabel('True Output')
 ax[1].set_ylabel('Predicted Output')
 
 pl.show()
-
-print(timestr_knn)
-print(timestr_gp)
